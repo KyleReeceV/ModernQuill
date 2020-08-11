3e426d0234a5a050fa303f9fa4af840511e022c3
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-08-11 18:29:27 -0400

    added customer bean and controller

--- backend/ModernQuill/src/main/java/com/mq/controllers/CustomerController.java
+++ backend/ModernQuill/src/main/java/com/mq/controllers/CustomerController.java
@@ -1,5 +1,22 @@
 package com.mq.controllers;
 
-public class CustomerController {
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mq.entities.Customer;
+
+@Component
+@Controller
+public class CustomerController {
+	
+	@RequestMapping(value = "/customers/{cid}", method = RequestMethod.GET)
+	@ResponseBody
+	public Customer getCustomerbyId(@PathVariable int cid) {
+		return null; // do implementation later
+	}
+	
 }
