054a104c3e16761bc525f4fa09fac67f46cb2431
Amanda <cosentinoamanda7@gmail.com>
2020-08-12 14:04:49 -0700

    Finished pen vertical

--- backend/ModernQuill/src/main/java/com/mq/controllers/LoginController.java
+++ backend/ModernQuill/src/main/java/com/mq/controllers/LoginController.java
@@ -1,5 +1,25 @@
 package com.mq.controllers;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mq.entities.Customer;
+
+@Component
+@RestController
 public class LoginController {
 
+	@RequestMapping(value="/login",method= RequestMethod.GET)
+	public Customer login() {
+		return null;
+	}
+	
+	@RequestMapping(value="/getUserInfo", method=RequestMethod.GET)
+	public Customer userInfo() {
+		return null;
+	}
+	
 }
--- backend/ModernQuill/src/main/java/com/mq/services/PenService.java
+++ backend/ModernQuill/src/main/java/com/mq/services/PenService.java
@@ -1,5 +1,14 @@
 package com.mq.services;
 
-public interface PenService {
+import java.util.List;
 
+import com.mq.entities.Pen;
+
+public interface PenService {
+	
+	Pen getPenById(int id);
+	List<Pen> getAllPens();
+	List<Pen> getPensByName(String name);
+	
+	List<Pen> getPenOrderAscending(boolean ascending);
 }
