8c061a9c53135f1cc0530ab70ff34083fafb06ae
schneicw <schneicw@whitman.edu>
2020-08-14 13:44:43 -0600

    login page design

--- frontend/mq-frontend/src/app/app.module.ts
+++ frontend/mq-frontend/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ShopPageComponent } from './shop-page/shop-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatFormFieldModule} from '@angular/material/form-field/';
+// import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -16,8 +22,17 @@ import { CartPageComponent } from './cart-page/cart-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    
+    // AngularMaterialModule,
+    MatSliderModule,
+    MatFormFieldModule
+    
   ],
+
+  
   providers: [],
   bootstrap: [AppComponent]
 })
