55d67ae34cf4bb81cf7df933fae6243349469a85
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-08-14 16:08:08 -0400

    Charles' REAL work

--- frontend/mq-frontend/src/app/app.module.ts
+++ frontend/mq-frontend/src/app/app.module.ts
@@ -12,25 +12,13 @@ import {MatDialogModule} from '@angular/material/dialog';
 
 
 
-import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ShopPageComponent } from './shop-page/shop-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatSliderModule } from '@angular/material/slider';
-import {MatFormFieldModule} from '@angular/material/form-field/';
-// import { AngularMaterialModule } from './angular-material.module';
-=======
 import { LoginPageComponent } from '../app/components/login-page/login-page.component';
 import { ShopPageComponent } from '../app/components/shop-page/shop-page.component';
 import { CartPageComponent } from '../app/components/cart-page/cart-page.component';
 import { PenService } from 'src/app/services/pen.service';
 import { PenDialogComponent } from './components/pen-dialog/pen-dialog.component'
->>>>>>> master
 
 @NgModule({
   declarations: [
@@ -43,19 +31,6 @@ import { PenDialogComponent } from './components/pen-dialog/pen-dialog.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    BrowserAnimationsModule,
-    HttpClientModule,
-    
-    // AngularMaterialModule,
-    MatSliderModule,
-    MatFormFieldModule
-    
-  ],
-
-  
-  providers: [],
-=======
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
@@ -67,7 +42,6 @@ import { PenDialogComponent } from './components/pen-dialog/pen-dialog.component
     MatDialogModule
   ],
   providers: [ PenService ],
->>>>>>> master
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/mq-frontend/src/app/components/login-page/login-page.component.ts
+++ frontend/mq-frontend/src/app/components/login-page/login-page.component.ts
@@ -12,6 +12,4 @@ export class LoginPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
-
 }
