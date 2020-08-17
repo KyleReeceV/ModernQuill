f84e8055e4d0a3a9855c4b32fe79dc74be8a3c72
schneicw <schneicw@whitman.edu>
2020-08-16 21:34:25 -0600

    added login stuff

--- frontend/mq-frontend/src/app/components/login-page/login-page.component.ts
+++ frontend/mq-frontend/src/app/components/login-page/login-page.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer} from '../../models/customer';
+import {LoginService} from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
+
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService:LoginService, private router:Router) { }
+
+  userField:string;
+  passField:string;
 
   ngOnInit(): void {
+  }
+
+  async loginCust(){
+    console.log(this.userField);
+
+    let cust = new Customer(this.userField,this.passField);
+    let customer = await this.loginService.loginUser(cust);
+    // console.log(customer);
+
+    if (customer === null){
+      alert("user not found")
+      this.router.navigateByUrl("/login");
+      this.userField="";
+      this.passField="";
+      this.router.navigateByUrl("/login");
+      
+    }
+    else{
+      this.router.navigateByUrl("/shop");
+      this.loginService.setCustomer(customer);
+      console.log(this.loginService.currCust)
+    }
+
+
   }
 
 }
