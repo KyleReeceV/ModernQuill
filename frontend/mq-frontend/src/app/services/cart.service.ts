f8eddda5133b7c2f06ee73d225333e39fafdf973
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-08-19 16:17:16 -0400

    replaced localhost API with EC2 API

--- frontend/mq-frontend/src/app/services/cart.service.ts
+++ frontend/mq-frontend/src/app/services/cart.service.ts
@@ -13,21 +13,21 @@ export class CartService {
   async createAllCartItems (carts: Array<Cart>) : Promise<Array<Cart>>{
 
     const cartsReturned:Array<Cart> = await this.http.post<Array<Cart>>
-              (`http://localhost:8080/carts/`, carts).toPromise();
+              (`http://ec2-3-91-229-143.compute-1.amazonaws.com:8080/carts/`, carts).toPromise();
     return cartsReturned;
   }
 
   async getAllCartsByCartId (cartId: number): Promise <Array<Cart>>{
     
     const cartsReturned:Array<Cart> = await this.http.get<Array<Cart>>
-    (`http://localhost:8080/carts/${cartId}`).toPromise();
+    (`http://ec2-3-91-229-143.compute-1.amazonaws.com:8080/carts/${cartId}`).toPromise();
     return cartsReturned;
 
   }
 
   async getLastElementId(): Promise<number>{
     const lastCartId:number = await this.http.get<number>
-    (`http://localhost:8080/carts/last-element`).toPromise();
+    (`http://ec2-3-91-229-143.compute-1.amazonaws.com:8080/carts/last-element`).toPromise();
 
     return lastCartId;
 
