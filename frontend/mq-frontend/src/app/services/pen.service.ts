dabab9dfc9c31256d7f32f2737f65333f0d353da
Kyle Van Sprewenburg <kvansprewenburg@radford.edu>
2020-08-21 16:31:44 -0400

    fixed service api calls

--- frontend/mq-frontend/src/app/services/pen.service.ts
+++ frontend/mq-frontend/src/app/services/pen.service.ts
@@ -11,27 +11,27 @@ export class PenService {
   constructor(private http:HttpClient) { }
 
   // async getAllPens():Promise<Array<Pen>> {
-  //   const pens:Array<Pen> = await this.http.get<Array<Pen>>(`http://ec2-3-91-229-143.compute-1.amazonaws.com:8080/pens/`).toPromise();
+  //   const pens:Array<Pen> = await this.http.get<Array<Pen>>(`http://ec2-100-25-157-201.compute-1.amazonaws.com:8080/pens/`).toPromise();
   //   return pens;
   // }
   
   async getAllPensSort(ascending:boolean):Promise<Array<Pen>> {
-    const pens:Array<Pen> = await this.http.get<Array<Pen>>(`http://ec2-3-91-229-143.compute-1.amazonaws.com:8080/pens?ascending=${ascending}`).toPromise();
+    const pens:Array<Pen> = await this.http.get<Array<Pen>>(`http://ec2-100-25-157-201.compute-1.amazonaws.com:8080/pens?ascending=${ascending}`).toPromise();
     return pens;
   }
 
   async getAllPens():Promise<Array<Pen>>{
-    const pens:Array<Pen> = await this.http.get<Array<Pen>>(`http://ec2-3-91-229-143.compute-1.amazonaws.com:8080/pens`).toPromise();
+    const pens:Array<Pen> = await this.http.get<Array<Pen>>(`http://ec2-100-25-157-201.compute-1.amazonaws.com:8080/pens`).toPromise();
     return pens;
   }
 
   // async getPenById(id:number):Promise<Pen> {
-  //   const pen:Pen = await this.http.get<Pen>(`http://ec2-3-91-229-143.compute-1.amazonaws.com:8080/pens/${id}`).toPromise();
+  //   const pen:Pen = await this.http.get<Pen>(`http://ec2-100-25-157-201.compute-1.amazonaws.com:8080/pens/${id}`).toPromise();
   //   return pen;
   // }
 
   async getPenById(id:number):Promise<Pen> {
-    const pen:Pen = await this.http.get<Pen>(`http://ec2-3-91-229-143.compute-1.amazonaws.com:8080/pens/${id}`).toPromise();
+    const pen:Pen = await this.http.get<Pen>(`http://ec2-100-25-157-201.compute-1.amazonaws.com:8080/pens/${id}`).toPromise();
     return pen;
   }
 
